9240e66f3adee67cc94b25eda9af290b04088fb9
truongthanh <truongthanh.bk56@gmail.com>
2017-09-13 17:35:30 +0900

    install gem config

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :user_id, prensence: true
-  validates :title, prensence: true, length: {maximum: 140}
-  validates :content, prensence: true, length: {maximum: 1440}
+  validates :title, prensence: true, length: {maximum: Setting.micropost.tittle_length}
+  validates :content, prensence: true, length: {maximum: Setting.micropost.content_length}
 end
